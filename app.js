cfbb7a90e90d81d09ba2fe9d3ec40d17b8ea28b2
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2019-12-17 15:16:28 -0600

    Adding user validation

--- app.js
+++ app.js
@@ -15,18 +15,25 @@ btnGeneratePW.addEventListener("click", () => {
   let passwordLength = prompt(
     "Please enter password length (Minimum: 8) (Maximum: 128) characters"
   );
-  let hasSymbol = confirm("Add symbols to password?");
-  let hasNumber = confirm("Add numeric characters to password?");
-  let hasLowerCase = confirm("Add lowercase characters to password?");
-  let hasUpperCase = confirm("Add uppercase characters to password?");
 
-  passwordEl.value = generatePassword(
-    passwordLength,
-    hasSymbol,
-    hasNumber,
-    hasLowerCase,
-    hasUpperCase
-  );
+  if (passwordLength == "") {
+    alert(
+      "Please enter a valid value! Example: Numbers ranging from (8 - 128)"
+    );
+  } else {
+    let hasSymbol = confirm("Add symbols to password?");
+    let hasNumber = confirm("Add numeric characters to password?");
+    let hasLowerCase = confirm("Add lowercase characters to password?");
+    let hasUpperCase = confirm("Add uppercase characters to password?");
+
+    passwordEl.value = generatePassword(
+      passwordLength,
+      hasSymbol,
+      hasNumber,
+      hasLowerCase,
+      hasUpperCase
+    );
+  }
 });
 
 // Generate password function
@@ -44,7 +51,7 @@ function generatePassword(length, symbol, number, lowercase, uppercase) {
 
   // Doesn't have a selected type
   if (typesCount === 0) {
-    return "";
+    alert("NO PASSWORD GENERATED: Please select a password character type");
   }
 
   // create a loop
