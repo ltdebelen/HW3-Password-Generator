4d0ccccc1279503ff2402ac37f372b8b585bca4b
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2019-12-18 20:30:32 -0600

    Modifying code for generating password

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const btnGeneratePW = document.getElementById("generate");
 const btnCopyPW = document.getElementById("copy");
 const passwordEl = document.getElementById("password");
 
-const randomFunc = {
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-  uppercase: getRandomLower,
-  lowercase: getRandomUpper
-};
-
-// Event Listener for Generate password button
 btnGeneratePW.addEventListener("click", () => {
   let passwordLength = prompt(
     "Please enter password length (Minimum: 8) (Maximum: 128) characters"
   );
 
-  if (passwordLength == "") {
+  if (passwordLength == "" || passwordLength < 8 || passwordLength > 128) {
     alert(
       "Please enter a valid value! Example: Numbers ranging from (8 - 128)"
     );
@@ -36,35 +28,43 @@ btnGeneratePW.addEventListener("click", () => {
   }
 });
 
-// Generate password function
 function generatePassword(length, symbol, number, lowercase, uppercase) {
   let parsedLength = parseInt(length);
   let generatedPassword = "";
   const typesCount = lowercase + uppercase + number + symbol;
 
-  const typesArr = [
-    { lowercase },
-    { uppercase },
-    { number },
-    { symbol }
-  ].filter(item => Object.values(item)[0]);
-
-  // Doesn't have a selected type
+  // This ensures that the user selects at least 1 password character type
   if (typesCount === 0) {
-    alert("NO PASSWORD GENERATED: Please select a password character type");
+    alert(
+      "NO PASSWORD GENERATED: Please select atleast one password character type"
+    );
+    return "";
+  } else {
+    // Need to push FALSE conditions into an Array
+    let passwordCondition = [symbol, number, lowercase, uppercase];
+    let falseConditions = [];
+    let element = false;
+    let idx = passwordCondition.indexOf(element);
+    while (idx != -1) {
+      falseConditions.push(idx);
+      idx = passwordCondition.indexOf(element, idx + 1);
+    }
+
+    // Loop through FALSE conditions and remove it from the Function Array
+    for (var i = falseConditions.length - 1; i >= 0; i--) {
+      myFunctions.splice(falseConditions[i], 1);
+    }
+
+    // Loop through the length of the required password
+    for (var i = 0; i < parsedLength; i++) {
+      let randomFunctionIndex = Math.floor(Math.random() * myFunctions.length);
+
+      // need to build string from calling the functions randomly
+      generatedPassword =
+        generatedPassword + myFunctions[randomFunctionIndex]();
+    }
+    return generatedPassword;
   }
-
-  // create a loop
-  for (let i = 0; i < parsedLength; i += typesCount) {
-    typesArr.forEach(type => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunc[funcName]();
-    });
-  }
-
-  const finalPassword = generatedPassword.slice(0, length);
-
-  return finalPassword;
 }
 
 // Copy password to clipboard
@@ -85,22 +85,24 @@ btnCopyPW.addEventListener("click", () => {
 });
 
 // Functions
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+const myFunctions = [
+  function getRandomSymbol() {
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  },
 
-function getRandomNumber() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
+  function getRandomNumber() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  },
 
-function getRandomLower() {
-  const alphabetsLower = "abcdefghijklmnopqrstuvwxyz";
-  return alphabetsLower[Math.floor(Math.random() * alphabetsLower.length)];
-}
+  function getRandomLower() {
+    const alphabetsLower = "abcdefghijklmnopqrstuvwxyz";
+    return alphabetsLower[Math.floor(Math.random() * alphabetsLower.length)];
+  },
 
-function getRandomUpper() {
-  const alphabetsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return alphabetsUpper[Math.floor(Math.random() * alphabetsUpper.length)];
-}
+  function getRandomUpper() {
+    const alphabetsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return alphabetsUpper[Math.floor(Math.random() * alphabetsUpper.length)];
+  }
+];
